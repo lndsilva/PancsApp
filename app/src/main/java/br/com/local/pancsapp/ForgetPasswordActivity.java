cf70452d859c76a204b210294866b0950b595311
lndsilva <lndsilva@hotmail.com>
2023-11-23 15:05:38 -0300

    tela de criar senha

---
+++ app/src/main/java/br/com/local/pancsapp/ForgetPasswordActivity.java
@@ -0,0 +1,14 @@
+package br.com.local.pancsapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ForgetPasswordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.forget_password_layout);
+    }
+}
--- app/src/main/java/br/com/local/pancsapp/LoginActivity.java
+++ app/src/main/java/br/com/local/pancsapp/LoginActivity.java
@@ -9,7 +9,8 @@ import android.view.View;
 import com.google.android.material.button.MaterialButton;
 
 public class LoginActivity extends AppCompatActivity {
-    MaterialButton btnLogin;
+    MaterialButton btnLogin, idForgetPassword;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,11 +18,19 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.login_layout);
 
         btnLogin = findViewById(R.id.btnLogin);
+        idForgetPassword = findViewById(R.id.idForgetPassword);
 
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(getApplicationContext(), MenuActivity.class));
+            }
+        });
+
+        idForgetPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), ForgetPasswordActivity.class));
             }
         });
     }
